e23e5362b633f39ba81a9fc9d206da61c6cc1fe1
nikunj4516 <nikbariya4516@gmail.com>
2026-05-25 18:26:23 +0530

    Use better sugarcane crop emoji

--- frontend/src/pages/ProfileSetup.tsx
+++ frontend/src/pages/ProfileSetup.tsx
@@ -110,7 +110,7 @@ const ProfileSetup = () => {
     <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Seedling.png" alt="sprout" className="w-5 h-5 drop-shadow-sm" key="sprout" />,
     <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Cloud.png" alt="cloud" className="w-5 h-5 drop-shadow-sm" key="cloud" />,
     <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Food/Ear%20of%20Corn.png" alt="bean" className="w-5 h-5 drop-shadow-sm" key="bean" />,
-    <Trees className="w-5 h-5 text-emerald-700" key="trees" />,
+    <span className="text-xl leading-none" aria-hidden="true" key="sugarcane">🎋</span>,
     <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Food/Carrot.png" alt="carrot" className="w-5 h-5 drop-shadow-sm" key="carrot" />,
     <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Objects/Package.png" alt="package" className="w-5 h-5 drop-shadow-sm" key="package" />
   ];
